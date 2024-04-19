47f310ab4cd909eb881191636bce9f95f1a2dae7
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2024-04-19 12:27:53 -0500

    remove git stash/apply fron pull script b/c it's not needed

--- git_pull.sh
+++ git_pull.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Simple bash script for making dealing with the weird git permissions issue on GCP more convenient
-# Automatically changes file permissions, stashes changes, pulls updates, and reapplies local changes 
+# Automatically changes file permissions then pulls updates
 
 # Exit bash script on error
 set -e
@@ -10,6 +10,4 @@ set -e
 USER=$(whoami)
 sudo chown -R $USER:$USER .
 
-git stash
 git pull
-git stash apply
