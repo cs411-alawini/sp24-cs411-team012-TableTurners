28e8ef6dca1a39b248d409029adfc34e70f9c7ab
jeremyl6 <jeremyl6@illinois.edu>
2024-04-23 17:39:35 -0500

    Added InputText elements for Login page. Need to work on figuring out whether it actually works.

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button } from 'primereact/button';
-import { PrimeIcons } from 'primereact/api';
-
-import api from '../../api/api';
-import { PageProps } from '../../Pages';
+import { useState } from 'react';											// React hook used for managing state within functional components
+import { useNavigate } from 'react-router-dom';				// React hook for navigation within the application, provided by React Router DOM
+import { Button } from 'primereact/button';						// React button UI component
+import { PrimeIcons } from 'primereact/api';					// Provides set of icon names
+import { InputText } from 'primereact/inputtext';			// React input text box UI component
+import api from '../../api/api';											// Imports API given by project
+import { PageProps } from '../../Pages';							// Contains props passed to pages in application
 
 function Login({ toast }: PageProps) {
   const navigate = useNavigate();
@@ -31,8 +31,27 @@ function Login({ toast }: PageProps) {
 
   return (
     <>
-      <h1>Login</h1>
-      <Button
+      <h1> Login </h1>
+		<div>
+			{ /* InputText element for Email Box */ }
+			<span className = "p-input-icon-left" >
+				<i className = "pi pi-address-book" style = {{ paddingLeft: '5px' }} />
+				<InputText 
+					placeholder = "Email" 
+					style={{ paddingLeft: '30px' }} />
+			</span>
+		</div>
+		<div>
+			{/* InputText element for Password box*/}
+			<span className = "p-input-icon-left">
+				<i className = "pi pi-lock" style = {{ paddingLeft: '5px' }}/>
+				<InputText 
+					placeholder = "Password" 
+					style={{ paddingLeft: '30px' }} />
+			</span>	
+		</div>
+	  { /* Button element to initiate login*/ }
+	  <Button
         icon={PrimeIcons.SIGN_IN}
         onClick={() => submit('root3544@Zyxel.com', '1234') /* Replace this with proper submit call with user input */}
         loading={loading}
