1f0e2981acda64cf3735ed67ee4b5a37d95d423f
jeremyl6 <jeremyl6@illinois.edu>
2024-04-23 19:09:19 -0500

    Made some changes to types bc API is being weird, also renamed email and password objects

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -8,14 +8,28 @@ import { PageProps } from '../../Pages';							// Contains props passed to pages
 
 function Login({ toast }: PageProps) {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] 		= useState( false );
+	const [inputEmail, setInputEmail]: [string, React.Dispatch<React.SetStateAction<string>>]	= useState( '' );			// Holds input email value, and defines function to handle changes to element
+	const [inputPassword, setInputPassword]: [string, React.Dispatch<React.SetStateAction<string>>] = useState( '' );			// Holds input password value, and defines function to handle changes to element
 
-  function submit(email: string, password: string) {
+	/* Sets the const email upon new text being entered into the corresponding text box */
+	function handleEmailChange( event: React.ChangeEvent<HTMLInputElement> ) {		
+		setInputEmail( event.target.value )
+	}
+
+	/* Sets the const password upon new text being entered into the corresponding text box */
+	function handlePasswordChange( event: React.ChangeEvent<HTMLInputElement> ) {
+		setInputPassword( event.target.value )
+	}
+
+	/* Submits email and password to API for authentication */
+  function submit() {
     setLoading(true);
     api
-      .post_login(email, password)
+      .post_login(inputEmail, inputPassword)
       .then((authenticated) => {
-        if (!authenticated) {
+        console.log( authenticated );
+				if (!authenticated) {
           // notify user of incorrect email/password
           return;
         }
@@ -29,6 +43,7 @@ function Login({ toast }: PageProps) {
       .finally(() => setLoading(false));
   }
 
+	/* Below contains the Frontend Elements */
   return (
     <>
       <h1> Login </h1>
@@ -38,7 +53,10 @@ function Login({ toast }: PageProps) {
 				<i className = "pi pi-address-book" style = {{ paddingLeft: '5px' }} />
 				<InputText 
 					placeholder = "Email" 
-					style={{ paddingLeft: '30px' }} />
+					style={{ paddingLeft: '30px' }}
+					value 			= { inputEmail }
+					onChange		= { handleEmailChange }
+				/>
 			</span>
 		</div>
 		<div>
@@ -47,13 +65,17 @@ function Login({ toast }: PageProps) {
 				<i className = "pi pi-lock" style = {{ paddingLeft: '5px' }}/>
 				<InputText 
 					placeholder = "Password" 
-					style={{ paddingLeft: '30px' }} />
+					style 			= { { paddingLeft: '30px' } }
+					value 			= { inputPassword }
+					onChange		= { handlePasswordChange }
+				/>
 			</span>	
 		</div>
 	  { /* Button element to initiate login*/ }
+		{/*'root3544@Zyxel.com', '1234' is example email/pass*/}
 	  <Button
         icon={PrimeIcons.SIGN_IN}
-        onClick={() => submit('root3544@Zyxel.com', '1234') /* Replace this with proper submit call with user input */}
+        onClick={() => submit() /* Replace this with proper submit call with user input */}
         loading={loading}
       >
         Login
