71a8799cf9cdc4984e750f8d0904f7c16e27e9e9
jeremyl6 <jeremyl6@illinois.edu>
2024-04-25 14:55:18 -0500

    Minor comments added for understanding

--- frontend/src/api/post_login.ts
+++ frontend/src/api/post_login.ts
@@ -8,6 +8,7 @@ import axios from 'axios';
  * @returns true is successfully logged in, false otherwise
  */
 export default async function post_login(email: string, password: string): Promise<boolean> {
+  /* Submit data to API and wait for response */
   try {
     await axios({
       method: 'post',
@@ -15,11 +16,13 @@ export default async function post_login(email: string, password: string): Promi
       timeout: 2000,
       data: { email, password },
     });
+  /* Catch error if present */
   } catch (error) {
     // Incorrect email/password if 401, unknown error otherwise
     if (axios.isAxiosError(error) && error.response && error.response.status === 401) return false;
     throw error;
   }
 
+  /* No error, return true */
   return true;
 }
